253254264e20dace8f3bb4730267d104ee6e3061
Bug制造师 <106008893+BugMaker888@users.noreply.github.com>
2022-10-01 11:12:51 +0800

    Update sheep.py

--- sheep.py
+++ sheep.py
@@ -8,21 +8,21 @@ class Sheep():
 
     def __init__(self):
         self.seed = [0, 0, 0, 0]
-        self.js_code = open("shuffle.js",encoding='utf-8').read()
+        self.js_code = open("shuffle.js", encoding="utf-8").read()
         self.map_data_path = "./map_data.txt"
 
     def response(self, flow):
-        ''' 接口响应方法 '''
+        """ 接口响应方法 """
         if "map_info_ex" in flow.request.path:
             # 获取随机种子的接口，随机种子存储到self.seed中
             response = json.loads(flow.response.content)
             self.seed = response["data"]["map_seed"]
             self.make_map_data()
         elif "maps" in flow.request.path:
-            # 获取地图数据的接口，地图数据保存到map_data.txt文件
+            # 获取地图数据的接口，不解析第一关
             if "046ef1bab26e5b9bfe2473ded237b572" in flow.request.path:
-                # 跳过第一关的接口
                 return
+            # 保存地图数据
             response = json.loads(flow.response.content)
             with open(self.map_data_path, "w") as f:
                 f.write(json.dumps(response, indent=4))
@@ -30,13 +30,15 @@ class Sheep():
             self.make_map_data()
 
     def make_map_data(self):
-        ''' 制作地图数据 '''
+        """ 制作地图数据 """
 
         print("==========================================")
 
         # 判断地图文件是否存在
         if not os.path.exists(self.map_data_path):
             return
+
+        # 读取原始地图数据
         map_data = json.loads(open(self.map_data_path).read())
 
         # 根据"blockTypeData"字段按顺序生成所有类型的方块，存放到数组
@@ -49,15 +51,15 @@ class Sheep():
                 block_types.extend([i] * count)
 
         # 调用js方法将数组打乱，打乱后的结果和游戏的一样
-        block_types = execjs.compile(self.js_code).call('shuffle', block_types, self.seed)
+        block_types = execjs.compile(self.js_code).call("shuffle", block_types, self.seed)
         print(block_types)
 
-        # 将游戏的层级数据按层数排序
+        # 将游戏的层数排序
         level_data = map_data["levelData"]
         layers = list(level_data.keys())
         layers.sort(key=lambda x:int(x))
 
-        # 为了方便three.js按顺序读取层数，所以将排序后的层级保存起来
+        # 为了方便three.js按顺序读取层数，所以将层数保存起来
         map_data["layers"] = layers
 
         # 将打乱后的图案按顺序填充到每个方块的"type"字段里
