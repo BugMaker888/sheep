6f373fa498a6125e6214403649cccd6ab5c87ecf
longhuan1999 <2114467924@qq.com>
2022-10-08 16:56:01 +0800

    调整样式

--- html/sheep_map/index.php
+++ html/sheep_map/index.php
@@ -4,6 +4,7 @@
         <title>羊了个羊3D地图</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="main.css">
         <style>
             body {
                 background-color: #cff998;
