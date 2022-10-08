eed6987168294f6536b35a1fd8ccf510a13c790c
longhuan1999 <2114467924@qq.com>
2022-10-08 18:00:03 +0800

    更新 index.php

--- html/sheep_map/index.php
+++ html/sheep_map/index.php
@@ -35,15 +35,43 @@
         <!-- Remove this when import maps will be widely supported -->
         <script async src="https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js"></script>
         <?php
+            class MyDB extends SQLite3{
+                function __construct(){
+                    $this->open('../sheep_maps.db');
+                }
+            }
             if(is_array($_GET)&&count($_GET)>0){//判断是否有Get参数
                 if(isset($_GET["id"])){//判断所需要的参数是否存在，isset用来检测变量是否设置，返回true or false
                     $id = $_GET["id"];//存在
-                    echo "<script async src='./map_data.php?id=$id'></script>";
-                } else {
-                    echo "<script async src='./map_data.js'></script>";
+                    $db = new MyDB();
+                    if(!$db){
+                        echo "\t\t<script>window.alert('".$db->lastErrorMsg()."');</script>\n";
+                        //echo $db->lastErrorMsg();
+                    } else {
+                        echo "\t\t<script>console.log('成功打开数据文件');</script>\n";
+                        //echo "成功创建或打开数据文件\n";
+                    }
+                    $ret1 = $db->query("SELECT ID,MAP_INFO from MAPS WHERE ID = $id");
+                    $ret2 = $db->query("SELECT MIN(ID) from MAPS");
+                    while($row = $ret1->fetchArray()){
+                        $map_info = $row["MAP_INFO"];
+                        echo "\t\t<script async>\nconst map_data = $map_info;</script>\n";  
+                    }
+                    if($map_info == null){
+                        echo "\t\t<script>window.alert('没有查询到 id=$id 的关卡地图数据！关卡地图数据可能已过期或者id不正确!');</script>\n";
+                    }
+                    while($row = $ret2->fetchArray()){
+                        $min_id = $row["MIN(ID)"];
+                        if(($id - $min_id) >= 50){
+                            $ret3 = $db->exec("DELETE from MAPS where ID=$min_id;");
+                            if(!$ret3){
+                                echo "\t\t<script>window.alert('".$db->lastErrorMsg()."');</script>\n";
+                            } else {
+                                echo "\t\t<script>console.log('".$db->changes()."  成功删除数据');</script>\n";
+                            }
+                        }
+                    }
                 }
-            } else {
-                echo "<script async src='./map_data.js'></script>";
             }
         ?>
 
