2abbde289816394397133a4f3e48a8d57e15fe3f
longhuan1999 <2114467924@qq.com>
2022-10-14 13:19:19 +0800

    对提交的关卡地图数据进行校验

--- html/index.php
+++ html/index.php
@@ -6,8 +6,27 @@
   </head>
   <body>
     <?php
-      //header('Access-Control-Allow-Origin:*');  //支持全域名访问，不安全，部署后需要固定限制为客户端网址
-      //header('Access-Control-Allow-Headers:x-requested-with,content-type');  //响应头 请按照自己需求添加。
+      date_default_timezone_set("PRC");
+      $data_time = date('Y-m-d H:i:s');
+      function getRealIp()
+      {
+        $ip=false;
+        if(!empty($_SERVER["HTTP_CLIENT_IP"])){
+          $ip = $_SERVER["HTTP_CLIENT_IP"];
+        }
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+          if ($ip) { array_unshift($ips, $ip); $ip = FALSE; }
+          for ($i = 0; $i < count($ips); $i++) {
+            if (!eregi ("^(10│172.16│192.168).", $ips[$i])) {
+              $ip = $ips[$i];
+              break;
+            }
+          }
+        }
+        return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+      }
+      $ip = getRealIp();
       class MyDB extends SQLite3{
         function __construct(){
           $this->open('sheep_maps.db');
@@ -25,7 +44,9 @@
       $sql1 =<<<EOF
         CREATE TABLE IF NOT EXISTS MAPS(
           ID INTEGER PRIMARY KEY AUTOINCREMENT,
-          MAP_INFO TEXT
+          MAP_INFO TEXT,
+          IP TEXT,
+          DATA_TIME TEXT
         );
 EOF;
 
@@ -41,24 +62,32 @@ EOF;
       $req_json_str = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
       //echo gettype($req_json_str);
       if(strpos($req_json_str, "levelData") !== false && strpos($req_json_str, "layers") !== false){
-        $sql2 = "INSERT INTO MAPS (MAP_INFO) VALUES ('$req_json_str')";
-        $ret2 = $db->exec($sql2);
-        $ret3 = $db->query("SELECT MAX(ID) from MAPS");
-        if(!$ret2){
-          echo "<script>window.alert('".$db->lastErrorMsg()."');</script>\n\t";
-          //echo $db->lastErrorMsg();
-        } else {
-          echo "<script>console.log('成功插入数据');</script>\n\t";
-          //echo "成功插入数据\n";
-          while($row = $ret3->fetchArray() ){
-            $ID = $row["MAX(ID)"];
-            echo "<a href='/sheep_map?id=$ID'>访问当前关卡3D地图</a>\n\t";
-            echo "<script>window.location.href='/sheep_map?id=$ID';</script>\n";
+        $ret4 = $db->query("SELECT ID FROM MAPS WHERE MAP_INFO = '$req_json_str'");
+        while($row = $ret4->fetchArray()){
+          $ID = $row["ID"];
+        }
+        if($ID == null){
+          $sql2 = "INSERT INTO MAPS (MAP_INFO,IP,DATA_TIME) VALUES ('$req_json_str','$ip','$data_time')";
+          $ret2 = $db->exec($sql2);
+          $ret3 = $db->query("SELECT MAX(ID) from MAPS");
+          if(!$ret2){
+            echo "<script>window.alert('".$db->lastErrorMsg()."');</script>\n\t";
+            //echo $db->lastErrorMsg();
+          } else {
+            echo "<script>console.log('成功插入数据');</script>\n\t";
+            //echo "成功插入数据\n";
+            while($row = $ret3->fetchArray() ){
+              $ID = $row["MAX(ID)"];
+            }
           }
         }
-      } else {
+      }
+      if($ID == null){
         echo "<a href='/sheep_map'>访问示例3D地图</a>\n\t";
         echo "<script>window.location.href='/sheep_map';</script>\n";
+      } else {
+        echo "<a href='/sheep_map?id=$ID'>访问当前关卡3D地图</a>\n\t";
+        echo "<script>window.location.href='/sheep_map?id=$ID';</script>\n";
       }
       $db->close();
     ?>
--- html/sheep_map/map_data.php
+++ html/sheep_map/map_data.php
@@ -16,32 +16,29 @@
     if(is_array($_GET)&&count($_GET)>0){//判断是否有Get参数
         if(isset($_GET["id"])){//判断所需要的参数是否存在，isset用来检测变量是否设置，返回true or false
             $id = $_GET["id"];//存在
-            $ret1 = $db->query("SELECT ID,MAP_INFO from MAPS WHERE ID = $id");
-            while($row = $ret1->fetchArray()){
-                $map_info = $row["MAP_INFO"];
-                echo "const map_data = $map_info;\n";  
-            }
-            if($map_info == null){
-                echo "window.alert('没有查询到 id=$id 的关卡地图数据！关卡地图数据可能已过期或者id不正确!')\n";
-            }
-        } else {
-            $ret1 = $db->query("SELECT MAX(ID),MAP_INFO from MAPS");
-            while($row = $ret1->fetchArray()){
-                $map_info = $row["MAP_INFO"];
-                echo "const map_data = $map_info;\n";  
-            }
         }
-    } else {
+    }
+    if($id == null){
         $ret1 = $db->query("SELECT MAX(ID),MAP_INFO from MAPS");
         while($row = $ret1->fetchArray()){
             $map_info = $row["MAP_INFO"];
+            $id = $row["MAX(ID)"];
             echo "const map_data = $map_info;\n";  
+        }
+    } else {
+        $ret1 = $db->query("SELECT ID,MAP_INFO from MAPS WHERE ID = $id");
+        while($row = $ret1->fetchArray()){
+            $map_info = $row["MAP_INFO"];
+            echo "const map_data = $map_info;\n";  
+        }
+        if($map_info == null){
+            echo "window.alert('没有查询到 id=$id 的关卡地图数据！关卡地图数据可能已过期或者id不正确!')\n";
         }
     }
     $ret2 = $db->query("SELECT MIN(ID) from MAPS");
     while($row = $ret2->fetchArray()){
         $min_id = $row["MIN(ID)"];
-        if(($id - $min_id) >= 50){
+        if(($id - $min_id) >= 100){
             $ret3 = $db->exec("DELETE from MAPS where ID=$min_id;");
             if(!$ret3){
                 echo "window.alert('".$db->lastErrorMsg()."');\n";
@@ -194,14 +191,14 @@ function init() {
     // 方块图案
     const textureLoader = new THREE.TextureLoader();
     material_side = new THREE.MeshLambertMaterial({
-        map: textureLoader.load('images/side.png'),
+        map: textureLoader.load('sheep_images/side.png'),
     })
     for (let i = 0; i <= 16; i++) {
         material_blocks.push(new THREE.MeshLambertMaterial({
-            map: textureLoader.load(`images/${i}.png`),
+            map: textureLoader.load(`sheep_images/${i}.png`),
         }));
         mask_material_blocks.push(new THREE.MeshLambertMaterial({
-            map: textureLoader.load(`images/mask/${i}.png`),
+            map: textureLoader.load(`sheep_images/mask/${i}.png`),
         }));
     }
 
